5cd12fe31cabd1ab1c7ebcf6e1c7c6b04747deb3
Gregorio Sigala <gregoriosigala@Gregorios-MBP-2.attlocal.net>
2023-03-05 08:20:04 -0700

    starting the game loop

--- GamePanel.java
+++ GamePanel.java
@@ -2,6 +2,9 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 
 public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
@@ -32,8 +35,29 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        while(gameThread !=  null){
+            //update the information of the game such as character position 
+            update();
+            //draw the screen with the updated information
+            repaint();
+        }
         
+    }
+
+    public void update(){
+        
+
+    } 
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
+
     }
 
     
