7342862290b7cbbafb5685aaa2988b84a3649c60
Gregorio Sigala <gregoriosigala@Gregorios-MBP-2.attlocal.net>
2023-03-05 22:43:10 -0700

    added the game loop & keynoard inputs

--- GamePanel.java
+++ GamePanel.java
@@ -19,33 +19,111 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenHeight = tileSize * maxScreenRow; 
     //576 px
 
+    //fps
+    int FPS = 60;
+    KeyHandler  keyH = new KeyHandler();
     Thread gameThread;
 
+    //set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
     public GamePanel(){
+
         this.setPreferredSize( new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener( keyH);
+        //game panel can be focused to recieve input
+        this.setFocusable(true);
+
     }
 
 
     public void startGameThread(){
+
         gameThread = new Thread(this);
         gameThread.start();
+
     }
+
+    // @Override
+    // public void run() {
+        
+    //     //0.0166
+    //     double drawInterval = 1000000000/FPS; 
+    //     double nextDrawTime = System.nanoTime() + drawInterval;
+    //     while(gameThread !=  null){
+            
+    //         //update the information of the game such as character position 
+    //         update();
+    //         //draw the screen with the updated information
+    //         repaint();
+
+    //         try {
+    //             double remainingTime = nextDrawTime - System.nanoTime();
+    //             remainingTime = remainingTime/1000000;
+
+    //             if(remainingTime < 0){
+    //                 remainingTime = 0;
+    //             }
+
+    //             Thread.sleep((long) remainingTime);
+    //             nextDrawTime += drawInterval;
+
+    //         } catch (InterruptedException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+        
+    // }
 
     @Override
-    public void run() {
-        while(gameThread !=  null){
-            //update the information of the game such as character position 
-            update();
-            //draw the screen with the updated information
-            repaint();
+    public void run(){
+        double drawInterval = 1000000000/FPS; 
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer =0;
+        int drawCount =0;
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += currentTime - lastTime;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount ++;
+
+            }
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount =0;
+                timer =0;
+
+            }
+
         }
-        
-    }
+}
 
     public void update(){
-        
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed){
+            playerX += playerSpeed;
+        }
 
     } 
 
@@ -55,10 +133,9 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
 
     }
-
     
 }
